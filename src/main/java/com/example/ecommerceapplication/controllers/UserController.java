e881937bb9a349ca05d21dde1ee5745e99dff1dc
Ahsanali059 <AhsanDev.org@gmail.com>
2024-04-27 23:37:52 +0500

    added get all users endpoint for admin

--- src/main/java/com/example/ecommerceapplication/controllers/UserController.java
+++ src/main/java/com/example/ecommerceapplication/controllers/UserController.java
@@ -1,14 +1,35 @@
 package com.example.ecommerceapplication.controllers;
 
+import com.example.ecommerceapplication.constants.AppConstants;
+import com.example.ecommerceapplication.dto.UserResponse;
+import com.example.ecommerceapplication.services.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 @SecurityRequirement(name = "E-Commerce Application")
-public class UserController
-{
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/admin/users")
+    public ResponseEntity<UserResponse> getUsers(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_USERS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+
+      UserResponse users = userService.getAllUsers(pageNumber, pageSize, sortBy, sortOrder);
+      return new ResponseEntity<UserResponse>(new UserResponse(), HttpStatus.FOUND);
+
+    }
 
 
 }
