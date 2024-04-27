5e3883ef1172ec82349cd950a60cd5f74490d0e4
Ahsanali059 <AhsanDev.org@gmail.com>
2024-04-27 23:25:30 +0500

    added register user method

--- src/main/java/com/example/ecommerceapplication/services/userServiceImpl.java
+++ src/main/java/com/example/ecommerceapplication/services/userServiceImpl.java
@@ -1,16 +1,26 @@
 package com.example.ecommerceapplication.services;
 
+import com.example.ecommerceapplication.constants.AppConstants;
+import com.example.ecommerceapplication.dto.AddressDTO;
 import com.example.ecommerceapplication.dto.UserDTO;
 import com.example.ecommerceapplication.dto.UserResponse;
+import com.example.ecommerceapplication.entities.Address;
+import com.example.ecommerceapplication.entities.Cart;
+import com.example.ecommerceapplication.entities.Role;
+import com.example.ecommerceapplication.entities.User;
+import com.example.ecommerceapplication.exceptions.APIException;
 import com.example.ecommerceapplication.repositories.AddressRepo;
 import com.example.ecommerceapplication.repositories.RoleRepo;
 import com.example.ecommerceapplication.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.ecommerceapplication.services.CartService;
+
+import java.util.List;
 
 @Transactional
 @Service
@@ -26,11 +36,56 @@ public class userServiceImpl implements UserService
     private AddressRepo addressRepo;
 
     @Autowired
+    private CartService cartService;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
-    public UserDTO registerUser(UserDTO userDTO) {
-        return null;
+    public UserDTO registerUser(UserDTO userDTO)
+    {
+        try
+        {
+            User user = modelMapper.map(userDTO, User.class);
+            Cart cart = new Cart();
+            user.setCart(cart);
+
+            Role role = roleRepo.findById(AppConstants.USER_ID).get();
+            user.getRoles().add(role);
+
+            String country = userDTO.getAddress().getCountry();
+            String state = userDTO.getAddress().getState();
+            String city = userDTO.getAddress().getCity();
+            String pincode = userDTO.getAddress().getPincode();
+            String street = userDTO.getAddress().getStreet();
+            String buildingName = userDTO.getAddress().getBuildingName();
+
+            Address address = addressRepo.findByCountryAndStateAndCityAndPincodeAndStreetAndBuildingName(country, state,
+                    city, pincode, street, buildingName);
+
+            if (address == null) {
+                address = new Address(country, state, city, pincode, street, buildingName);
+
+                address = addressRepo.save(address);
+            }
+
+            user.setAddresses(List.of(address));
+            User savedUser = userRepo.save(user);
+            cart.setUser(savedUser);
+            userDTO = modelMapper.map(savedUser, UserDTO.class);
+            userDTO.setAddress(modelMapper.map(user.getAddresses().stream().findFirst().get(), AddressDTO.class));
+
+            return userDTO;
+
+        }
+        catch (DataIntegrityViolationException e)
+        {
+            throw new APIException("User already exists with emailId: " + userDTO.getEmail());
+        }
+
     }
 
     @Override
