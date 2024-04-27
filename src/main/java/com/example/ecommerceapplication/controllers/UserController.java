26e520c74e66b8c69293ae019ace3c9d5cc5e0da
Ahsanali059 <AhsanDev.org@gmail.com>
2024-04-27 23:33:40 +0500

    added user controller

---
+++ src/main/java/com/example/ecommerceapplication/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.example.ecommerceapplication.controllers;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@SecurityRequirement(name = "E-Commerce Application")
+public class UserController
+{
+
+
+}
