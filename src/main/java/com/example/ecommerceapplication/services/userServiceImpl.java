29696ab9f8cb216f852270b49cc1f173e263b7fb
Ahsanali059 <AhsanDev.org@gmail.com>
2024-04-27 22:43:07 +0500

    added fileUpload userService

--- src/main/java/com/example/ecommerceapplication/services/userServiceImpl.java
+++ src/main/java/com/example/ecommerceapplication/services/userServiceImpl.java
@@ -1,16 +1,20 @@
 package com.example.ecommerceapplication.services;
 
+import com.example.ecommerceapplication.dto.UserDTO;
+import com.example.ecommerceapplication.dto.UserResponse;
 import com.example.ecommerceapplication.repositories.AddressRepo;
 import com.example.ecommerceapplication.repositories.RoleRepo;
 import com.example.ecommerceapplication.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.ecommerceapplication.services.CartService;
 
+@Transactional
 @Service
-public class userServiceImpl
+public class userServiceImpl implements UserService
 {
     @Autowired
     private UserRepository userRepo;
@@ -21,9 +25,33 @@ public class userServiceImpl
     @Autowired
     private AddressRepo addressRepo;
 
-
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDTO registerUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public UserResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public UserDTO getUserById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserDTO updateUser(Long userId, UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public String deleteUser(Long userId) {
+        return "";
+    }
 
 
     /*
